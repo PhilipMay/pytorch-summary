c8e61e0d72708a17f10289613e744c566a6ec16a
Philip May <eniak.info@gmail.com>
2020-04-14 21:16:55 +0200

    Update torchsummary.py

--- torchsummary/torchsummary.py
+++ torchsummary/torchsummary.py
@@ -22,7 +22,9 @@ def summary_string(model, input_size, batch_size=-1, device=torch.device('cuda:0
 
     def register_hook(module):
         def hook(module, input, output):
+            print(type(output))
             output = output['linear']
+            print(type(output))
             class_name = str(module.__class__).split(".")[-1].split("'")[0]
             module_idx = len(summary)
 
